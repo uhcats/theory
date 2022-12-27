add5047c9cc0d1b77ed2b295fb18158cdf9e8332
Stachu <stas1111pl@wp.pl>
2022-12-27 19:01:16 +0100

    changing version

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Servers is listening at http://localhost:3000/');
+  console.log('Servers is listening at http://localhost:3000/ Let\'s play a game!');
 });
 
 app.get('/', function (req, res) {
