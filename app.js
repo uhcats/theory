4c6539c8dcd3d3184b94103ec1cb164f61d9ce10
Stachu <stas1111pl@wp.pl>
2023-01-02 19:20:58 +0100

    add login and register page

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log('Servers is listening at http://localhost:3000/ Let\'s play a game!');
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+app.use(express.static("public"));
+
+
+app.listen(PORT || process.env, () => {
+  console.log('Server is listening at http://localhost:3000');
+
 });
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-});
 
-app.use(express.static(
-  path.join(__dirname, 'public'),
-));
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
